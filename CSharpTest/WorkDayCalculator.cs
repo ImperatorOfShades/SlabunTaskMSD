829ae7143a63d405c9eb538cd31579009f1f276d
Levko Slabun <slabun2012@gmail.com>
2020-02-20 23:48:00 +0200

    Добавьте файлы проекта.

---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            DateTime enddate;
+            if (weekEnds != null)
+            {
+                TimeSpan dateDifference;
+                int comparation, weekEndsDuration;
+                enddate = startDate.AddDays(dayCount - 1);
+                foreach (var weekEnd in weekEnds)
+                {
+                    comparation = DateTime.Compare(enddate, weekEnd.StartDate);
+                    if (comparation >= 0)
+                    {
+                        dateDifference = weekEnd.EndDate - weekEnd.StartDate;
+                        weekEndsDuration = dateDifference.Days + 1;
+                        enddate = enddate.AddDays(weekEndsDuration);
+                    }
+                }
+            }
+            else
+                enddate = startDate.AddDays(dayCount - 1);
+
+            return enddate;
+
+            throw new NotImplementedException();
+        }
+    }
+}
